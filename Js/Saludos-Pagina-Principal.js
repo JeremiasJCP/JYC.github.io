e064ba7a331a8b47ed66efc12fc6a3baccbfae59
JeremiasJCP <jerecordero17@gmail.com>
2023-11-09 00:47:30 +0100

    Gran avance en la seccion sobre nosotros

--- Js/Saludos-Pagina-Principal.js
+++ Js/Saludos-Pagina-Principal.js
@@ -1,25 +1,21 @@
 var fecha = new Date();
 var hoy  =  fecha.toLocaleDateString()
   
-function mostrarSaludo_imagenFondo() { 
+function mostrarSaludo() { 
 
     
     var hora = fecha.getHours(); 
-    var imagenFondo = ""; 
     var mensaje = "";
   
   if (hora >= 6 && hora < 12) { 
     mensaje = "¡Buenos días, mi princesa!";
-    imagenFondo = "https://i.ibb.co/3mH2vbQ/claro.jpg"; // Imagen de fondo para la mañana 
   } 
   
   else if (hora >= 12 && hora < 18) { 
     mensaje = "¡Buenas tardes, mi amor!";
-    imagenFondo = "https://i.ibb.co/PMtYwXJ/medio.jpg"; // Imagen de fondo para la tarde 
 } 
   else { 
     mensaje = "¡Buenas noches, mol mio!";
-    imagenFondo = "https://i.ibb.co/f1WYZKs/oscuro.jpg";
      // Imagen de fondo para la noche 
   }
   
@@ -29,8 +25,6 @@ function mostrarSaludo_imagenFondo() {
   // var contenedorImagen = document.getElementById("contenedorPrincipal");
   //  contenedorImagen.style.backgroundImage = "url('" + imagenFondo + "')"; 
 }
-
-mostrarSaludo_imagenFondo()
 
 function proximoAniversario() {
   let spanProximoAniversario = document.getElementById('numeroProximoAniversario')
@@ -76,5 +70,6 @@ function diasQueFaltanParaVernos() {
   console.log(posicionObjetoLista) //Esta va a hacer la salida al HTML
 }
 
+mostrarSaludo()
 diasQueFaltanParaVernos();
 proximoAniversario()
